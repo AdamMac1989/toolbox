852027cf291f0a71ecffceba04ec4180550e34e6
Adam Macphee <amacph22@caledonian.ac.uk>
2018-02-25 16:14:29 +0000

    Updated completewood.php to run the deletion. Updated required stock to show required tools and required screws.

--- Schema/userscrews.sql
+++ Schema/userscrews.sql
@@ -40,13 +40,65 @@ CREATE TABLE `user_screw` (
 --
 
 INSERT INTO `user_screw` (`screw_id`, `length`, `number`, `user_id`) VALUES
-('test05', 2, 20, 'test05'),
-('TEST99', 45, 45, 'TEST99'),
-('sc008', 2, 10, 'user01'),
 ('sc009', 2, 10, 'user01'),
 ('sc0010', 2, 10, 'user01'),
 ('sc0010', 4, 20, 'user01'),
 ('sc0010', 4, 20, 'user01');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Feb 25, 2018 at 02:46 PM
+-- Server version: 5.6.34-log
+-- PHP Version: 7.1.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `toolbox`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_wood`
+--
+
+CREATE TABLE `user_wood` (
+  `wood_id` varchar(10) NOT NULL,
+  `length` int(12) NOT NULL,
+  `width` int(12) NOT NULL,
+  `height` int(12) NOT NULL,
+  `user_id` varchar(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_wood`
+--
+
+INSERT INTO `user_wood` (`wood_id`, `length`, `width`, `height`, `user_id`) VALUES
+('WD001', 20, 20, 20, 'USER01'),
+('test06', 20, 20, 20, 'test06'),
+('WD002', 20, 20, 20, 'USER01'),
+('WD008', 50, 50, 5, 'USER01');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- api/completeproject.php
+++ api/completeproject.php
@@ -9,7 +9,6 @@ require_once 'model/userscrews.php';
 
 require_once 'model/projectscrews.php';
 
-
 //if (!isset($_GET['id']) || empty($_GET['id'])) {
 //    echo "You did not pass in an ID.";
 //    exit;
@@ -31,6 +30,33 @@ foreach ($screws as $screw) {
 
     $userscrews->deleteScrew($screw ['length'], $screw['number']);
 }
+
+//for wood see below
+
+require_once 'model/userwood.php';
+
+require_once 'model/projectwood.php';
+
+
+$projectwood = new ProjectWood();
+$woods = $projectwood->getWood($_GET['p_name']);
+
+$userwood = new UserWood();
+
+
+if ($woods === false) {
+    echo "Topic not found!";
+    exit;
+}
+
+foreach ($woods as $wood) {
+    print_r($wood);
+
+    $userwood->deleteWood($wood ['length'], $wood['width'], $wood['height']);
+}
+
+//no change made below.
+
 
 
 header("Location: ../view/Customeraccountpage.htm");
